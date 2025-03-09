4023bdd7790c1f2b1933de012a218a82cc6fbbc2
Saurav Mishra <saurav24242424@gmail.com>
2025-03-10 01:09:14 +0530

    json data updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(express.json());
